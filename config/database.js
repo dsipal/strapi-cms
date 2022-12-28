27cd474a9c9d5681dfe9d12ca90a7c266bacd0c5
dsipal <devin@devinsipal.com>
2022-12-28 20:06:07 +0000

    changes to db

--- config/database.js
+++ config/database.js
@@ -1,20 +1,19 @@
-// strapi-cms/config/database.js
-var parseDbUrl = require("parse-database-url")
-var dbConfig = parseDbUrl(process.env["DATABASE_URL"])
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: dbConfig.host,
-      port: dbConfig.port || 5432,
-      database: dbConfig.database,
-      user: dbConfig.user,
-      password: dbConfig.password,
-      schema: dbConfig.schema, // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password
       },
+      debug: false,
     },
-    debug: true,
-  },
-});
+  };
+};
