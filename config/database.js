fd2789c629d0d2cc65a42564ea4ec0f9d42d2067
dsipal <devin@devinsipal.com>
2022-12-28 21:48:38 +0000

    change database info

--- config/database.js
+++ config/database.js
@@ -1,24 +1,11 @@
-const { parse } = require("pg-connection-string");
+const path = require('path');
 
-module.exports = ({ env }) => {
-  var { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  if(port==null){
-    port = 5432
-  }
-
-  console.log({ host, port, database, user, password })
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-      },
-      debug: false,
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
