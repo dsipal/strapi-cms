bc613de1e1ada4988b6a458ea4c9b0a791350e4b
Devin Sipal <devin@devinsipal.com>
2023-01-20 17:09:52 -0500

    fix saving icon name

--- src/plugins/fontawesome-strapi/admin/src/components/IconField/index.js
+++ src/plugins/fontawesome-strapi/admin/src/components/IconField/index.js
@@ -1,45 +1,33 @@
-import React, { useState, useEffect} from 'react'
+import React from 'react'
 import { Grid, GridItem } from '@strapi/design-system/Grid'
 import { Box } from '@strapi/design-system/Box'
 import { Searchbar, SearchForm } from '@strapi/design-system'
-import { Tooltip } from '@strapi/design-system/Tooltip'
 import { Typography } from '@strapi/design-system/Typography'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { set } from 'lodash'
 import { useIntl } from 'react-intl';
-import {useQueryParams } from '@strapi/helper-plugin';
 
 config.autoAddCss = false
 library.add(fas)
 
-
-
 const Field = ({
     name,
     onChange,
-    required
+    required,
+    value
 }) => {
-  const [{ query, rawQuery }] = useQueryParams();
-  const [value, setValue] = useState(query && query._q ? query._q : '');
   const { formatMessage } = useIntl();
   const iconsArray = Object.keys(fas)
   .filter(icon => {
     if (icon.includes(value)) {
-      return true
+      return icon
     }
-    //return icon
   }).sort()
 
-  const handleClear = () => {
-    setValue('');
-  };
-
-  const handleChange = (n, v) => {
-    const nextValue = set(value, n, v);
-    onChange({target: {name, value: JSON.stringify(nextValue), type: 'json'}})
+  const handleChange = (e, newValue) => {
+    onChange({target: { name, value: e.target.value || newValue, type: "string"}})
   }
 
   return (
@@ -47,14 +35,15 @@ const Field = ({
       <Box paddingBottom={8}>
         <SearchForm>
           <Box>
-            <Typography paddingBottom={9}>Icons</Typography>
+            <Typography>Currently Selected Icon: </Typography>
+            <FontAwesomeIcon icon={fas[value]} size="2x" inverse></FontAwesomeIcon>
           </Box>
           <Searchbar
           name="icon-search"
           value={value}
           clearLabel={formatMessage({ id: 'clearLabel', defaultMessage: 'Clear'})}
-          onChange = {e => setValue(e.target.value)}
-          onClear={handleClear}
+          onChange = {e => handleChange(e)}
+          onClear={e => handleChange(e, '')}
           placeholder="Search"
           required={required}
           >
@@ -63,27 +52,17 @@ const Field = ({
         </SearchForm>
       </Box>
 
-      <Box height="200px" overflow="scroll">
+      <Box height="200px" overflow="scroll" background={'neutral0'}>
         <Grid>
           {iconsArray.map(icon => {
             return (
               <GridItem
               padding={2}
               col={2}
-              key={icon}
-              background={'neutral0'}>
+              key={icon}>
                 <Box
-                    onClick={(e) => { setValue(icon)
-                      // const arg = {
-                      //   target: {
-                      //     name,
-                      //     value: icon
-                      //   }
-                      // }
-                      // console.log(arg)
-                      // onChange(arg)
-                    }}>
-                      <FontAwesomeIcon icon={fas[icon]} />
+                    onClick={(e) => handleChange(e, icon)}>
+                      <FontAwesomeIcon icon={fas[icon]} inverse/>
                     </Box>
               </GridItem>
             )
@@ -93,4 +72,5 @@ const Field = ({
     </>
   )
 }
+
 export default Field
