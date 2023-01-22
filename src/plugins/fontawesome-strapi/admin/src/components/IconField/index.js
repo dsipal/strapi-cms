216006c6e85cf23a7bf1bd8dbe5d63d8c3d4ac7c
Devin Sipal <devin@devinsipal.com>
2023-01-22 05:23:51 -0500

    plugin works with multiple sources, faster search

--- src/plugins/fontawesome-strapi/admin/src/components/IconField/index.js
+++ src/plugins/fontawesome-strapi/admin/src/components/IconField/index.js
@@ -1,16 +1,68 @@
-import React from 'react'
+import React, {useCallback,useState,useEffect,useMemo} from 'react'
 import { Grid, GridItem } from '@strapi/design-system/Grid'
 import { Box } from '@strapi/design-system/Box'
-import { Searchbar, SearchForm } from '@strapi/design-system'
+import { Searchbar, SearchForm, Button } from '@strapi/design-system'
 import { Typography } from '@strapi/design-system/Typography'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useIntl } from 'react-intl';
 
 config.autoAddCss = false
-library.add(fas)
+
+const sources = [fas,fab,far]
+const label_dict = {
+  "fas": "fa-solid",
+  "fab": "fa-brands",
+  "far": "fa-regular"
+}
+
+sources.forEach(source => {
+  library.add(source)
+})
+
+//function to generate icon string from icon
+const getIconString = (icon) => {
+  const label = label_dict[icon.prefix]
+  const iconName = icon.iconName
+  return String(`${label} ${iconName}`)
+}
+
+const arrayFromSource = (source) => {
+  console.log("making array from source")
+  return Object.values(source).map((icon) => {
+    return {
+      "icon": icon, 
+      "string":getIconString(icon),
+      "prefix": icon.prefix,
+      "name": icon.iconName
+    }
+  })
+}
+
+//function to create new single array from all
+//entries in sources with icon object as key and iconString as value
+const makeList = (sources) => {
+  let x = []
+  sources.forEach(source => {
+    const y = arrayFromSource(source)
+    x = x.concat(y)
+  })
+  return x
+}
+
+const CurrentIcon = (value) => {
+  if (typeof value.value === "string" && value.value.split(" ").length === 2) {
+    const [prefix, iconName] = value.value.split(" ");
+    const icon = { prefix, iconName };
+    return (<FontAwesomeIcon icon={icon} size="2x" inverse></FontAwesomeIcon>)
+  } else {
+    return <Typography>None</Typography>
+  }
+}
 
 const Field = ({
     name,
@@ -18,16 +70,43 @@ const Field = ({
     required,
     value
 }) => {
-  const { formatMessage } = useIntl();
-  const iconsArray = Object.keys(fas)
-  .filter(icon => {
-    if (icon.includes(value)) {
-      return icon
-    }
-  }).sort()
+  const iconList = useMemo(() => makeList(sources),[])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(36)
+  const [iconsToDisplay, setIconsToDisplay] = useState([])
+  const { formatMessage } = useIntl()
+  const [totalPages, setTotalPages] = useState(0)
 
-  const handleChange = (e, newValue) => {
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+    const iconsArray = Object.entries(iconList)
+    .filter(([key,icon]) => {
+      if (icon.string.includes(value)) {
+        return true
+      }
+    }).sort()
+    const iconsToDisplay = iconsArray.slice(startIndex, endIndex)
+    setIconsToDisplay(iconsToDisplay)
+    const totalPages = Math.ceil(iconsArray.length / itemsPerPage)
+    setTotalPages(totalPages)
+  }, [currentPage, itemsPerPage, iconList, value]);
+  
+  const handleChange = useCallback((e, newValue) => {
     onChange({target: { name, value: e.target.value || newValue, type: "string"}})
+  }, [onChange, name])
+
+  const makeElem = ([key,icon]) => {
+    return (
+      <GridItem
+      padding={2}
+      col={2}
+      key={key}>
+        <Box onClick={(e) => handleChange(e, icon.string)}>
+              <FontAwesomeIcon icon={icon.icon} inverse/>
+            </Box>
+      </GridItem>
+    )
   }
 
   return (
@@ -35,8 +114,9 @@ const Field = ({
       <Box paddingBottom={8}>
         <SearchForm>
           <Box>
-            <Typography>Currently Selected Icon: </Typography>
-            <FontAwesomeIcon icon={fas[value]} size="2x" inverse></FontAwesomeIcon>
+            <Typography>Current Icon: </Typography>
+            {/* <FontAwesomeIcon icon={value} size="2x" inverse></FontAwesomeIcon> */}
+            <CurrentIcon value={value} />
           </Box>
           <Searchbar
           name="icon-search"
@@ -52,21 +132,15 @@ const Field = ({
         </SearchForm>
       </Box>
 
-      <Box height="200px" overflow="scroll" background={'neutral0'}>
+      <Box height="200px" overflow="hidden" background={'neutral0'}>
         <Grid>
-          {iconsArray.map(icon => {
-            return (
-              <GridItem
-              padding={2}
-              col={2}
-              key={icon}>
-                <Box
-                    onClick={(e) => handleChange(e, icon)}>
-                      <FontAwesomeIcon icon={fas[icon]} inverse/>
-                    </Box>
-              </GridItem>
-            )
-          })}
+          {iconsToDisplay.map(makeElem)}
+        </Grid>
+      </Box>
+      <Box>
+        <Grid>
+          <GridItem><Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>Previous</Button></GridItem>
+          <GridItem><Button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages || iconsToDisplay.length === 0}>Next</Button></GridItem>
         </Grid>
       </Box>
     </>
